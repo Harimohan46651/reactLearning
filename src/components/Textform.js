671d7db068bcec1c53692ef8e4f18258cbfd7354
Harimohan <harimohan.h@prod.hclpnp.com>
2023-09-03 18:39:45 +0530

    lecture8
    added some features to textUtils.
    key points: background color change, add word counter, char counter, some color changes and spacing between buttons

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -8,19 +8,34 @@ export default function Textform(props) {
         let convertedText = text.toUpperCase()
         setText(convertedText) 
     }
+    const handleLoClick = ()=>{
+        console.log("on change clicked")
+        let convertedText = text.toLowerCase()
+        setText(convertedText) 
+    }
     const handleOnChange = (event)=>{
         console.log("on change clicked")
         setText(event.target.value)
     }
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-        <textarea className="form-control" value = {text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+    <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+            <textarea className="form-control" value = {text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button onClick={handleUpClick} className="btn btn-primary mx-2">Change to Uppercase</button>
+            <button onClick={handleLoClick} className="btn btn-primary mx-2">Change to lowercase</button>
+
+
         </div>
-        <button onClick={handleUpClick} className="btn btn-primary">Change to Uppercase</button>
-
-
-    </div>
+        <div className='container my-3'>
+        <h1>Your Text summary</h1>
+        <p>you have entered {text.split(' ').length} word and {text.length} characters </p>
+        <p> estimated time to read: {0.008*text.split(' ').length}</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+        </div>
+    </>
   )
 }
